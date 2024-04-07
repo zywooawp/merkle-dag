76d3c2275a1c858f71d9edc4127bc452db2d7ccf
zywooawp <2962922428@qq.com>
2024-04-07 18:36:47 +0800

    kk

--- dag.go
+++ dag.go
@@ -2,6 +2,11 @@ package merkledag
 
 import "hash"
 
+const (
+	K          = 1 << 10
+	BLOCK_SIZE = 256 * K
+)
+
 type Link struct {
 	Name string
 	Hash []byte
@@ -14,6 +19,61 @@ type Object struct {
 }
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
+	// 将分片写入到KVStore中，并返回Merkle Root
+	switch n := node.(type) {
+	case File:
+		return StoreFile(store, n, h)
+	case Dir:
+		return StoreDir(store, n, h)
+	}
+	return nil
+}
+
+func StoreFile(store KVStore, node File, h hash.Hash) []byte {
+	// 处理文件存储
+	t := []byte("blob")
+	if node.Size() > BLOCK_SIZE {
+		t = []byte("list")
+	}
+
+	data := node.Bytes()
+
+	// 将数据存储到KVStore中
+	err := store.Put(h.Sum(nil), data)
+	if err != nil {
+		// 处理错误
+		return nil
+	}
+
+	return h.Sum(nil)
+}
+
+func StoreDir(store KVStore, dir Dir, h hash.Hash) []byte {
+	// 处理文件夹存储
+	tree := Object{
+		Links: make([]Link, 0),
+		Data:  make([]byte, 0),
+	}
+
+	It := dir.It()
+	for It.Next() {
+		node := It.Node()
+
+		// 递归处理子文件/文件夹
+		linkHash := Add(store, node, h)
+		if linkHash != nil {
+			// 创建并添加链接到对象中
+			link := Link{
+				Name: node.Name(), // 假设Node接口有Name()方法来获取名称
+				Hash: linkHash,
+				Size: node.Size(),
+			}
+			tree.Links = append(tree.Links, link)
+		}
+	}
+
+	// 计算对象的哈希并将其存储到KVStore中
+	// 这里省略了计算哈希和存储的部分
+
 	return nil
 }
--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,59 @@
 package merkledag
 
-// Hash to file
+import (
+	"encoding/json"
+	"hash"
+	"strings"
+)
+
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
+	pathSegments := strings.Split(path, "/")
+	if len(pathSegments) == 0 {
+		return nil
+	}
+
+	objBytes, _ := store.Get(hash)
+
+	var obj Object
+	json.Unmarshal(objBytes, &obj)
+
+	return recursiveSearch(store, obj, pathSegments, hp)
+}
+
+func recursiveSearch(store KVStore, obj Object, pathSegments []string, hp HashPool) []byte {
+	if len(pathSegments) == 0 {
+		return obj.Data
+	}
+
+	for _, value := range obj.Links {
+		switch pathSegments[0] {
+		case "blob":
+			blobValue := CalHash2(value.Hash, hp.Get())
+			blobData, _ := store.Get(blobValue)
+			return blobData
+		case "link":
+			subObject := getObject(store, value.Hash)
+			return recursiveSearch(store, subObject, pathSegments, hp)
+		case "tree":
+			subObject := getObject(store, value.Hash)
+			return recursiveSearch(store, subObject, pathSegments[1:], hp)
+		}
+	}
+
 	return nil
+}
+
+func CalHash2(data []byte, h hash.Hash) []byte {
+	h.Reset()
+	hash := h.Sum(data)
+	h.Reset()
+	return hash
+}
+
+func getObject(store KVStore, hash []byte) Object {
+	objBytes, _ := store.Get(hash)
+
+	var obj Object
+	json.Unmarshal(objBytes, &obj)
+	return obj
 }
